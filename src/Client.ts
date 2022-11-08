e9274775ff054cca39b22dee195e8ecdc2bc5554
wioenena-q <wioenena@gmail.com>
2022-11-08 21:46:20 +0300

    chore: some minor spelling changes

--- src/Client.ts
+++ src/Client.ts
@@ -8,33 +8,33 @@ import { Client as BaseClient } from 'discord.js';
 export class Client extends BaseClient {
   #owners: Snowflake[];
 
-  public constructor ({ owners, token, clientOptions }: SnowyClientOptions) {
+  public constructor({ owners, token, clientOptions }: SnowyClientOptions) {
     super(clientOptions);
     this.token = token ?? null;
     this.#owners = owners ?? [];
   }
 
   /**
-	 * Checks if the user is an owner of the bot.
-	 * @param {UserResolvable} idOrInstance The ID or instance of the user to check.
-	 * @returns {boolean} Whether or not the user is an bot owner.
-	 */
-  public isOwner (idOrInstance: UserResolvable): boolean {
+   * Checks if the user is an owner of the bot.
+   * @param {UserResolvable} idOrInstance The ID or instance of the user to check.
+   * @returns {boolean} Whether or not the user is an bot owner.
+   */
+  public isOwner(idOrInstance: UserResolvable): boolean {
     const id = this.users.resolveId(idOrInstance);
 
-    return (id != null) ? this.#owners.includes(id) : false;
+    return id !== null ? this.#owners.includes(id) : false;
   }
 
   /**
-	 * @returns {Snowflake[]} The owners of the bot.
-	 */
-  public get owners (): Snowflake[] {
+   * @returns {Snowflake[]} The owners of the bot.
+   */
+  public get owners(): Snowflake[] {
     return this.#owners;
   }
 }
 
 export interface SnowyClientOptions {
-  token?: string
-  owners?: Snowflake[]
+  token?: string // Client token
+  owners?: Snowflake[] // Client owners
   clientOptions: ClientOptions
 }
