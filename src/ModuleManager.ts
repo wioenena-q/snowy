44ebb5179656317a965b62ce91e4923bd5f23c60
wioenena-q <wioenena@gmail.com>
2022-11-12 23:14:14 +0300

    feat: read module paths

--- src/ModuleManager.ts
+++ src/ModuleManager.ts
@@ -1,4 +1,6 @@
 import { EventEmitter } from 'node:events';
+import { readdirSync, type Dirent } from 'node:fs';
+import { join } from 'node:path';
 import type { SnowyModule } from './SnowyModule';
 import { UniqueMap } from './UniqueMap';
 
@@ -7,7 +9,44 @@ import { UniqueMap } from './UniqueMap';
  * @extends {EventEmitter}
  */
 export class ModuleManager extends EventEmitter {
-	#modules = new UniqueMap<string, SnowyModule>();
+	#modules: UniqueMap<string, SnowyModule>;
+	#options: ModuleManagerOptions;
 
+	public constructor(options: ModuleManagerOptions) {
+		super();
+		this.#modules = new UniqueMap<string, SnowyModule>();
+		this.#options = options;
+	}
+
+	/**
+	 * Reads the module directory and returns the file paths of the modules.
+	 * @returns {string[]} The paths of the modules.
+	 */
+	public getModuleFilePaths(): string[] {
+		const result = [] as string[];
+
+		; (function readDir(path: string) {
+			const files = readdirSync(path, { withFileTypes: true });
+			files.forEach((file: Dirent) => {
+				if (file.isDirectory()) readDir(join(path, file.name));
+				else result.push(join(path, file.name));
+			});
+		})(this.#options.path);
+
+		return result;
+	}
+
+	/**
+	 * @returns {UniqueMap<string, SnowyModule>} The modules of the bot.
+	 */
 	public get modules(): UniqueMap<string, SnowyModule> { return this.#modules; }
+
+	/**
+	 * @returns {ModuleManagerOptions} The options of the manager.
+	 */
+	public get options(): ModuleManagerOptions { return this.#options; }
+}
+
+export interface ModuleManagerOptions {
+	path: string // The path to the modules.
 }
