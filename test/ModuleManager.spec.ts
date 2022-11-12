07806072bb315ec0e7c22a993775d7367712fedd
wioenena-q <wioenena@gmail.com>
2022-11-12 23:14:44 +0300

    test: getModuleFilePaths method tested successfully

--- test/ModuleManager.spec.ts
+++ test/ModuleManager.spec.ts
@@ -1,8 +1,13 @@
+import { describe, expect, test } from '@jest/globals';
 import { join } from 'node:path';
 import { ModuleManager } from '../src/ModuleManager';
 
-const mngr = new ModuleManager({
+const manager = new ModuleManager({
 	path: join(__dirname, 'modules')
 });
 
-console.log(mngr.getModuleFilePaths());
+describe('ModuleManager', () => {
+	test('module manager successfully read module directory', () => {
+		expect(manager.getModuleFilePaths().length).toBeGreaterThan(0);
+	});
+});
