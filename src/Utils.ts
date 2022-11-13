3a959d023f46f864087100abe2b7e5055974e2f5
wioenena-q <wioenena@gmail.com>
2022-11-13 21:33:55 +0300

    feat: utilities created

---
+++ src/Utils.ts
@@ -0,0 +1,8 @@
+export const isObject = <T extends object>(value: unknown): value is T => {
+	return !Array.isArray(value) && typeof value === 'object' && value !== null;
+};
+
+export const isFunction = (value: unknown): value is (...args: unknown[]) => unknown =>
+	typeof value === 'function';
+
+export const isString = (value: unknown): value is string => typeof value === 'string';
