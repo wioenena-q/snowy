d225243431c38cc610cfe05d00d698d92d9ab719
wioenena-q <wioenena@gmail.com>
2022-11-17 23:39:46 +0300

    feat: some added new utility functions

--- src/Utils.ts
+++ src/Utils.ts
@@ -3,11 +3,19 @@ export const isObject = <T extends object>(value: unknown): value is T => {
 	return !Array.isArray(value) && typeof value === 'object' && value !== null;
 };
 
-export const isFunction = (value: unknown): value is (...args: unknown[]) => unknown =>
+export const isFunction = <R>(value: unknown): value is (...args: unknown[]) => R =>
 	typeof value === 'function';
 
 export const isString = (value: unknown): value is string => typeof value === 'string';
 
+export const getType = (value: unknown, arrType?: string): string => {
+	if (Array.isArray(value)) return `Array<${arrType ?? 'unknown'}>`;
+	if (value === null) return 'null';
+	return typeof value;
+};
+
 export type MaybeArray<T> = T | T[];
 
 export type Nullable<T> = T | null;
+
+export type MaybePromise<T> = T | Promise<T>;
