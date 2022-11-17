b50a0e9e8cbaaa37e809b1b6a9fb7a4cafe4143d
wioenena-q <wioenena@gmail.com>
2022-11-17 20:15:06 +0300

    feat: Basic CommandManager class implemented

--- src/Commands/CommandManager.ts
+++ src/Commands/CommandManager.ts
@@ -1,7 +1,29 @@
-import { ModuleManager } from '../ModuleManager';
+import type { Client } from '../Client';
+import { ModuleManager, ModuleManagerOptions } from '../ModuleManager';
 
 /**
  * @classdesc The manager class for the bot commands.
  * @extends {ModuleManager}
  */
-export class CommandManager extends ModuleManager { }
+export class CommandManager extends ModuleManager {
+	public constructor(client: Client, options: CommandManagerOptions) {
+		super(client, options);
+	}
+
+	public override async loadModules(filter?: (_path: string) => boolean): Promise<this> {
+		await super.loadModules(filter);
+		this.context.client.once('ready', () => {
+			this.context.client.on('messageCreate', async (_message) => {
+				// TODO: Implement command handling.
+			});
+
+			this.context.client.on('interactionCreate', (_interaction) => {
+				// TODO: Implement slash command handling.
+			});
+		});
+		return this;
+	}
+}
+
+export interface CommandManagerOptions extends ModuleManagerOptions {
+}
