c2bd9f586cb84429a8bc9c651d8ff492bff8f6de
wioenena-q <wioenena@gmail.com>
2022-11-17 20:16:40 +0300

    feat: basic SlashCommand class implemented

--- src/Commands/SlashCommand.ts
+++ src/Commands/SlashCommand.ts
@@ -1,1 +1,26 @@
-// TODO: Implement SlashCommand class
+import type { SnowyContext } from '../SnowyContext';
+import { BaseCommand, BaseCommandOptions } from './BaseCommand';
+
+/**
+ * @classdesc The base class for all slash commands.
+ * @extends {SnowyModule}
+ */
+export abstract class SlashCommand extends BaseCommand {
+	public readonly name: string;
+
+	/**
+	 *
+	 * @param {SnowyContext} context The context of the command.
+	 * @param {string} id The ID of the command.
+	 * @param {SlashCommandOptions} options The options of the command.
+	 */
+	public constructor(context: SnowyContext, id: string, options: SlashCommandOptions) {
+		super(context, id, options);
+
+		this.name = options.name;
+	}
+}
+
+export interface SlashCommandOptions extends BaseCommandOptions {
+	name: string
+}
