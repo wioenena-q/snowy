04930a0e1f719781b544ce5c43d01c0f94441d1a
wioenena-q <wioenena@gmail.com>
2022-11-17 20:18:36 +0300

    feat: some tags added

--- src/SnowyError.ts
+++ src/SnowyError.ts
@@ -8,7 +8,8 @@ export enum ErrorTags {
   MODULE_DOES_NOT_HAVE_A_PATH = 'MODULE_DOES_NOT_HAVE_A_PATH',
   MODULE_NOT_FOUND = 'MODULE_NOT_FOUND',
   EMITTER_NOT_FOUND = 'EMITTER_NOT_FOUND',
-  INVALID_LISTENER_TYPE = 'INVALID_LISTENER_TYPE'
+  INVALID_LISTENER_TYPE = 'INVALID_LISTENER_TYPE',
+  InvalidArgument = 'InvalidArgument'
 }
 
 /**
@@ -42,10 +43,11 @@ const tagToMessage: Record<ErrorTags, (...params: string[]) => string> = {
     `The method '${method}' of the class '${cls}' is not implemented.`,
   [ErrorTags.VALUE_IS_NOT_OF_DESIRED_TYPE]: (type: string, value: string, received: string) =>
     `The value '${value}' is not an ${type}. Received type: ${received} and required type: ${type}.`,
-  [ErrorTags.VALUE_IS_NOT_INSTANCE_OF_DESIRED_CLASS]: (cls: string, value: string, received: string) =>
-    `The value '${value}' is not an instance of ${cls}. Received type: ${received} and required type: ${cls}.`,
+  [ErrorTags.VALUE_IS_NOT_INSTANCE_OF_DESIRED_CLASS]: (cls: string, value: string) =>
+    `The value '${value}' is not an instance of ${cls}.`,
   [ErrorTags.MODULE_DOES_NOT_HAVE_A_PATH]: (modID: string) => `The module '${modID}' does not have a path.`,
   [ErrorTags.MODULE_NOT_FOUND]: (modID: string) => `The module '${modID}' was not found.`,
   [ErrorTags.EMITTER_NOT_FOUND]: (emitterID: string) => `The emitter '${emitterID}' was not found.`,
-  [ErrorTags.INVALID_LISTENER_TYPE]: (type: string) => `The listener type '${type}' is invalid.`
+  [ErrorTags.INVALID_LISTENER_TYPE]: (type: string) => `The listener type '${type}' is invalid.`,
+  [ErrorTags.InvalidArgument]: (argName: string, reason: string) => `Invalid argument: ${argName}. ${reason}`
 };
