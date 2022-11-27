8d61c4fd6d6997a0a085a501b599150df6b19f6e
wioenena-q <wioenena@gmail.com>
2022-11-27 16:36:19 +0300

    feat: some utility functions created

--- src/Utils.ts
+++ src/Utils.ts
@@ -14,6 +14,17 @@ export const getType = (value: unknown, arrType?: string): string => {
 	return typeof value;
 };
 
+export const toTheCallable = (value: unknown): Function => {
+	if (isFunction(value)) return value;
+	const toTheCallableWrapper = () => value;;
+	return toTheCallableWrapper;
+};
+
+export const toTheArray = <T>(value: unknown): T[] => {
+	if (Array.isArray(value)) return value;
+	return [value as T];
+};
+
 export type MaybeArray<T> = T | T[];
 
 export type Nullable<T> = T | null;
