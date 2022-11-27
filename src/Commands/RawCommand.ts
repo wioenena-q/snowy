a76d60f1aeb1ca87d763a80c73094410b012d56f
wioenena-q <wioenena@gmail.com>
2022-11-27 16:31:23 +0300

    feat: some added features(guildOnly).

--- src/Commands/RawCommand.ts
+++ src/Commands/RawCommand.ts
@@ -1,7 +1,7 @@
 import type { Message, PermissionsString } from 'discord.js';
 import type { SnowyContext } from '../SnowyContext';
 import { ErrorTags, SnowyError } from '../SnowyError';
-import { getType, MaybeArray } from '../Utils';
+import { getType, MaybeArray, MaybePromise } from '../Utils';
 import { BaseCommand, BaseCommandOptions } from './BaseCommand';
 
 export abstract class RawCommand extends BaseCommand {
@@ -11,6 +11,19 @@ export abstract class RawCommand extends BaseCommand {
 	 * @type {string[]}
 	 */
 	public aliases: string[];
+
+	/**
+	 *
+	 * Whether to run the command in only one guild
+	 * @type {boolean}
+	 */
+	public guildOnly: boolean;
+
+	/**
+	 * The prefixes for the command.
+	 * @type {ValidPrefixesDefinitions}
+	 */
+	public prefix?: ValidPrefixesDefinitions | null;
 	/**
 	 *
 	 * The permissions of the user using the command.
@@ -37,6 +50,8 @@ export abstract class RawCommand extends BaseCommand {
 			throw new SnowyError(ErrorTags.VALUE_IS_NOT_OF_DESIRED_TYPE, 'Array<string>', 'aliases', getType(options.aliases));
 
 		this.aliases = options.aliases;
+		this.guildOnly = options.guildOnly ?? false;
+		this.prefix = options.prefix ?? null;
 		this.userPermissions = options.userPermissions ?? null;
 		this.botPermissions = options.botPermissions ?? null;
 	}
@@ -44,10 +59,16 @@ export abstract class RawCommand extends BaseCommand {
 
 export interface RawCommandOptions extends BaseCommandOptions {
 	aliases: string[]
+	guildOnly?: boolean
+	prefix?: ValidPrefixesDefinitions
 	userPermissions?: ValidPermissionDefinitions
 	botPermissions?: ValidPermissionDefinitions
 }
 
-export type RawCommandPermissionFunction = (message: Message) => boolean | Promise<boolean>;
+export type RawCommandPrefixFunction = (message: Message) => MaybePromise<MaybeArray<string>>;
+
+export type ValidPrefixesDefinitions = MaybeArray<string> | RawCommandPrefixFunction;
+
+export type RawCommandPermissionFunction = (message: Message) => MaybePromise<boolean>;
 
 export type ValidPermissionDefinitions = MaybeArray<PermissionsString> | RawCommandPermissionFunction;
